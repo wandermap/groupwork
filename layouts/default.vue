7400eed5a433d36b870fda8dfb9be6cafdb7f186
wandermap <the7pieceplot@gmail.com>
2021-03-21 09:08:03 +0900

    プロジェクト新規作成

---
+++ layouts/default.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <b-navbar class="is-primary">
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <b>グループワーク</b>
+        </b-navbar-item>
+      </template>
+      <template #start>
+        <b-navbar-item href="#"> 生徒 </b-navbar-item>
+        <b-navbar-item href="#"> 教師 </b-navbar-item>
+        <b-navbar-dropdown label="Info">
+          <b-navbar-item href="#"> About </b-navbar-item>
+          <b-navbar-item href="#"> Contact </b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
+
+      <template #end>
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <a class="button is-light"> 会員登録 </a>
+            <a class="button is-light"> ログイン </a>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+
+    <section class="main-content columns">
+      <aside class="column is-2 section">
+        <ul class="menu-list">
+          <li v-for="(item, key) of items" :key="key">
+            <nuxt-link :to="item.to" exact-active-class="is-active">
+              <b-icon :icon="item.icon" /> {{ item.title }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </aside>
+
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' },
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' },
+        },
+      ],
+    }
+  },
+}
+</script>
